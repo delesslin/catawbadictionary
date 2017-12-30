4da4ebd008a0021db0d7105288393b11fe94022f
delesslin <delesslingw@gmail.com>
2017-12-29 23:37:22 -0500

    Working Catawba language dictionary display

---
+++ data.js
@@ -0,0 +1,205 @@
+let icons = [
+	{
+		catawba: "payą",
+		english: "squirrel",
+		icon: "squirrel.png",
+		color: [133, 0, 133],
+		sound: "squirrel.mp3"
+	},
+	{
+		catawba: "tąsisurɛ",
+		english: "wolf",
+		icon: "wolf.png",
+		color: [0, 133, 133],
+		sound: "wolf.mp3"
+	},
+	{
+		catawba: "éé",
+		english: "crow",
+		icon: "crow.png",
+		color: [133, 133, 0],
+		sound: "crow.mp3"
+	},
+	{
+		catawba: "witabuye",
+		english: "deer",
+		icon: "deer.png",
+		color: [0, 0, 133],
+		sound: "deer.mp3"
+	},
+	{
+		catawba: "kayaɁ",
+		english: "turtle",
+		icon: "turtle.png",
+		color: [200, 100, 200],
+		sound: "turtle.mp3"
+	},
+	{
+		catawba: "watkąɁ",
+		english: "chicken",
+		icon: "chicken.png",
+		color: [100, 200, 133],
+		sound: "chicken.mp3"
+	},
+	{
+		catawba: "tama",
+		english: "acorn",
+		icon: "acorn.png",
+		color: [200, 100, 133],
+		sound: "acorn.mp3"
+	},
+	{
+		catawba: "dαbαwąɁ",
+		english: "rabbit",
+		icon: "rabbit.png",
+		color: [133, 200, 100],
+		sound: "rabbit.mp3"
+	},
+	{
+		catawba: "iinsui",
+		english: "skunk",
+		icon: "skunk.gif",
+		color: [255, 100, 133],
+		sound: "skunk.mp3"
+	},
+	{
+		catawba: "ičignɛ",
+		english: "raccoon",
+		icon: "raccoon.png",
+		color: [255, 50, 255],
+		sound: "raccoon.mp3"
+	},
+	{
+		catawba: "ahąɁ",
+		english: "goose",
+		icon: "goose.png",
+		color: [0, 200, 133],
+		sound: "goose.mp3"
+	},
+	{
+		catawba: "kus",
+		english: "corn",
+		icon: "corn.png",
+		color: [255, 200, 133],
+		sound: "corn.mp3"
+	},
+	{
+		catawba: "hαpkɛ",
+		english: "cat",
+		icon: "cat.png",
+		color: [200, 0, 100],
+		sound: "cat.mp3"
+	},
+	{
+		catawba: "wąks",
+		english: "possum",
+		icon: "possum.png",
+		color: [20, 155, 200],
+		sound: "possum.mp3"
+	},
+	{
+		catawba: "wačαk",
+		english: "woodpecker",
+		icon: "woodpecker.png",
+		color: [20, 155, 200],
+		sound: "woodpecker.mp3"
+	},
+	{
+		catawba: "wįtą",
+		english: "mouse",
+		icon: "mouse.png",
+		color: [20, 255, 200],
+		sound: "mouse.mp3"
+	},
+	{
+		catawba: "yapa",
+		english: "leaf",
+		icon: "leaf.png",
+		color: [200, 155, 100],
+		sound: "leaf.mp3"
+	},
+	{
+		catawba: "kučiri",
+		english: "worm",
+		icon: "worm.png",
+		color: [200, 155, 100],
+		sound: "worm.mp3"
+	},
+	{
+		catawba: "yaɁswą",
+		english: "rattlesnake",
+		icon: "rattlesnake.png",
+		color: [200, 155, 100],
+		sound: "rattlesnake.mp3"
+	},
+	{
+		catawba: "wiispαkpαk",
+		english: "robin",
+		icon: "robin.png",
+		color: [200, 155, 100],
+		sound: "robin.mp3"
+	},
+	{
+		catawba: "dαpęyamuhę",
+		english: "red fox",
+		icon: "redfox.png",
+		color: [200, 155, 100],
+		sound: "fox.mp3"
+	},
+	{
+		catawba: "hųhų",
+		english: "screech owl",
+		icon: "owl.png",
+		color: [200, 155, 100],
+		sound: "owl.mp3"
+	},
+	{
+		catawba: "peadure",
+		english: "canoe",
+		icon: "canoe.png",
+		color: [200, 155, 100],
+		sound: "canoe.mp3"
+	},
+	{
+		catawba: "wαs wamp",
+		english: "hornet",
+		icon: "hornet.png",
+		color: [200, 155, 100],
+		sound: "hornet.mp3"
+	},
+	{
+		catawba: "nɛmą",
+		english: "cloud",
+		icon: "cloud.png",
+		color: [200, 155, 100],
+		sound: "cloud.mp3"
+	},
+	{
+		catawba: "nimęɁ",
+		english: "bear",
+		icon: "bear.png",
+		color: [200, 155, 100],
+		sound: "bear.mp3"
+	},
+	{
+		catawba: "wičiči",
+		english: "eagle",
+		icon: "eagle.png",
+		color: [200, 155, 100],
+		sound: "eagle.mp3"
+	},
+	{
+		catawba: "arαrąy",
+		english: "frog",
+		icon: "frog.png",
+		color: [200, 155, 100],
+		sound: "frog.mp3"
+	},
+	{
+		catawba: "tąsi",
+		english: "dog",
+		icon: "dog.png",
+		color: [200, 155, 100],
+		sound: "dog.mp3"
+	}
+]
